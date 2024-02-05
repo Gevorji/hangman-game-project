127e0e2dfd4799ed95bf660ea787f5f1b13fdfc1
Gevorji <techno9882@gmail.com>
2024-02-05 20:43:02 +0300

    Add drawer class

---
+++ drawer.py
@@ -0,0 +1,35 @@
+class Drawer:
+    def __init__(self, frames_file):
+        self.frames = self.process_frames(frames_file)
+        self.frame_counter = 0
+
+    def process_frames(self, file):
+        frame_source = file.readlines()
+        frames = []
+        frame = []
+        for line in frame_source:
+            if line == 'delim\n':
+                frame = ''.join(frame)
+                frames.append(frame)
+                frame = []
+            else:
+                frame.append(line)
+        return frames
+
+    def draw_a_frame(self):
+        if self.frame_counter > len(self.frames)-1:
+            self.frame_counter = 0
+        else:
+            print(self.frames[self.frame_counter])
+            self.frame_counter += 1
+
+
+if __name__ == '__main__':
+    frames_file = open('body_art.txt', encoding='utf-8')
+    drawer = Drawer(frames_file=frames_file)
+    import os, time
+    for i in range(6):
+        drawer.draw_a_frame()
+        time.sleep(1)
+        os.system('cls')
+
