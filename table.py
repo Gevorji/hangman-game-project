705abfdd94e0d4bd7a7c48e1e266289bc7d7b80d
Gevorji <techno9882@gmail.com>
2024-02-06 21:10:48 +0300

    Add table.py

---
+++ table.py
@@ -0,0 +1,19 @@
+class Table:
+    def __init__(self, places_numb):
+        self.table_state = {k: ' ' for k in range(places_numb)}
+
+    def restart(self):
+        self.table_state = {}
+
+    def put_a_char(self, char, where):
+        self.table_state[where] = char
+
+    def display(self):
+        score = ' '.join(self.table_state.values())
+        under = ' '.join('_'*len(self.table_state))
+        return score  + '\n' + under
+
+if __name__ == '__main__':
+    tb = Table(7)
+    tb.put_a_char('a', 3)
+    print(tb.display())
